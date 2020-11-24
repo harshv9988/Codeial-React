f7a330470c16a5ebf9403a514c6ad04ae859bd06
harshv9988 <harshv9988@gmail.com>
2020-11-24 13:11:09 +0530

    fetching posts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./components/App";
 import { configureStore } from "./store/index";
 
 const store = configureStore();
-console.log("store", store);
+// console.log("store", store);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
